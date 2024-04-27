3ba73e56f40e9796a704519c81632941d6a54906
Josué Martínez <luzmariasanchez.studio@gmail.com>
2024-04-26 21:58:58 -0600

    Update flag.py

--- flag.py
+++ flag.py
@@ -1,13 +1,15 @@
 from time import sleep
 import vlc
-playing = set([1,2,3,4])  
+import keyboard
+
+playing = set([1, 2, 3, 4])
 video_directory = "/home/pi/Downloads/vis3.mp4"
 # creating Instance class object
 vlc_instance = vlc.Instance()
-  
+
 player = vlc_instance.media_player_new()
 player.set_fullscreen(True)
-  
+
 sleep(5)
 
 while True:
@@ -16,7 +18,10 @@ while True:
     sleep(1)
     while player.get_state() in playing:
         sleep(1)
-        continue        
+        # Check if 'q' is pressed to stop execution
+        if keyboard.is_pressed('q'):
+            player.stop()
+            print("Execution stopped by user")
+            exit()  # Exit the program
     print("Finished")
     sleep(5)
-    continue
